0a62442f20020775cb75b73179912927cb48b16d
TheOrbMaverick <enangita@gmail.com>
2024-09-09 14:45:39 +0100

    created the home screen view

--- src/planzoSite/urls.py
+++ src/planzoSite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from events.views import (home_screen_view,)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_screen_view, name='home'),
 ]
