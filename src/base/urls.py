7878d07075e44fa621c2cdbb0f6bc120e1f59830
bryansine <bryansine1738@gmail.com>
2024-09-11 14:45:06 +0300

    update on the homepage

--- src/base/urls.py
+++ src/base/urls.py
@@ -1,9 +1,10 @@
 from .views import home, about, petition_signup
 from django.urls import path
 
+
 urlpatterns = [
    
-    path('home/', home, name='home'),
+    path('', home, name='home'),
     path('about/', about, name='about'),
     path('petition_signup/', petition_signup, name='petition_signup'),
 ]
--- src/planzoSite/urls.py
+++ src/planzoSite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from base.views import home
 from django.urls import path, include
-from events.views import (home_screen_view,)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),
+    path('home/', home),
 ]
